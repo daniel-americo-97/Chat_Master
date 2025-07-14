9af1a83790a0f96c3ad6de6fdb68bd51379d8d91
daniel-americo-97 <daniel.americon@gmail.com>
2025-07-14 17:31:37 -0300

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const main = document.createElement("div");
+  main.style.marginTop = "30px";
+  main.innerHTML = `
+    <h2>Olá, Chat_Master!</h2>
+    <p>Seu projeto está rodando via GitHub Pages.<br>
+    Use este arquivo index.js para adicionar funcionalidades JS interativas.</p>
+  `;
+  document.body.appendChild(main);
+});
