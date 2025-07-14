5b047fc564eb4a2738ea0d2b6198fd129ed11bba
daniel-americo-97 <daniel.americon@gmail.com>
2025-07-14 17:00:38 -0300

    Teste Chat_Master
    Teste Chat_Master

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return <div>{message}</div>;
+}
+
+export default App;
